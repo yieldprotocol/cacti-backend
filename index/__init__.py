f4e70a59dea1958267ecff9c945392179d54544a
Kah Keng Tay <kahkeng@gmail.com>
2023-01-26 14:43:59 -0800

    index: hook up weaviate index and use it as default

--- index/__init__.py
+++ index/__init__.py
@@ -1,15 +1,22 @@
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores.faiss import FAISS
+from langchain.vectorstores import FAISS, Weaviate
 from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
+
+from .base import (
+    IndexVariant,
+)
 
 
 _docsearch = None
 
 
-def get_docsearch():
+def get_docsearch(index_variant: IndexVariant = IndexVariant.weaviate):
     global _docsearch
-    if _docsearch is None:
+    if _docsearch is not None:
+        return _docsearch
+
+    if index_variant == IndexVariant.faiss:
         embeddings = OpenAIEmbeddings()
         with open('./web3fuctions.txt') as f:
             web3functions = f.read()
@@ -18,4 +25,13 @@ def get_docsearch():
         documents = web3functions.split("---")
         #print(documents)
         _docsearch = FAISS.from_texts(documents, embeddings)
+
+    elif index_variant == IndexVariant.weaviate:
+        from index import weaviate
+        client = weaviate.get_client()
+        _docsearch = Weaviate(client, weaviate.INDEX_NAME, weaviate.TEXT_KEY)
+
+    else:
+        raise ValueError(f'unrecognized chat variant: {chat_variant}')
+
     return _docsearch
