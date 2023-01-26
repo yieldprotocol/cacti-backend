81ff7b6c7bd07804c5c7198b4800ad18feb7f924
Kah Keng Tay <kahkeng@gmail.com>
2023-01-26 14:45:18 -0800

    server: make it listen on 0.0.0.0 instead of 127.0.0.1

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ def message_received(client, server, message):
     server.send_message(client, r)
 
 
-server = WebsocketServer(host='127.0.0.1', port=9999, loglevel=logging.INFO)
+server = WebsocketServer(host='0.0.0.0', port=9999, loglevel=logging.INFO)
 server.set_fn_new_client(new_client)
 server.set_fn_message_received(message_received)
 server.set_fn_client_left(client_left)
