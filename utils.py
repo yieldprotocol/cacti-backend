617471320d2fab9148011f3869e44d775dc76360
Kah Keng Tay <kahkeng@gmail.com>
2023-02-02 16:51:15 -0800

    docker: add nginx proxy for https/wss

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ from langchain.llms import OpenAI
 
 
 OPENAI_API_KEY = "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"
-WEAVIATE_URL = "http://chatweb3:q0jficzXOA69T5FWgAeT@23.20.190.128:5050"
+WEAVIATE_URL = "https://chatweb3:q0jficzXOA69T5FWgAeT@chatweb3.func.ai:5050"
 
 
 
