da1fb9231b0b8e216aec44b05a845b06e7cac6b1
Kah Keng Tay <kahkeng@gmail.com>
2023-02-14 14:31:16 -0800

    system: refactor to instantiate from a config dict

--- chat/__init__.py
+++ chat/__init__.py
@@ -1,30 +1,6 @@
-import os
+from .base import ChatHistory, BaseChat
 
-import index
-
-from .base import (
-    BaseChat,
-    ChatVariant,
-)
-from .simple import SimpleChat
-from .rephrase import RephraseChat
-from .rephrase_cited import RephraseCitedChat
-from .widget_search import WidgetSearchChat
-
-
-DEFAULT_CHAT_VARIANT = ChatVariant.widget_search
-
-
-def new_chat(chat_variant: ChatVariant = DEFAULT_CHAT_VARIANT, show_intermediate_output: bool = True) -> BaseChat:
-    docsearch = index.get_docsearch()
-    if chat_variant == ChatVariant.simple:
-        return SimpleChat(docsearch)
-    elif chat_variant == ChatVariant.rephrase:
-        return RephraseChat(docsearch, show_rephrased=show_intermediate_output)
-    elif chat_variant == ChatVariant.rephrase_cited:
-        return RephraseCitedChat(docsearch, show_rephrased=show_intermediate_output)
-    elif chat_variant == ChatVariant.widget_search:
-        widget_search = index.get_widget_search()
-        return WidgetSearchChat(docsearch, widget_search, show_thinking=show_intermediate_output)
-    else:
-        raise ValueError(f'unrecognized chat variant: {chat_variant}')
+from . import simple
+from . import rephrase
+from . import rephrase_cited
+from . import widget_search
