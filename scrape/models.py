0d78e3e2954e6ada43addc5cb45dc5d9e772bbde
Kah Keng Tay <kahkeng@gmail.com>
2023-02-18 22:38:54 -0800

    scrape: move to shared postgres db

--- scrape/models.py
+++ scrape/models.py
@@ -12,11 +12,14 @@ from sqlalchemy import (  # type: ignore
 from sqlalchemy.orm import (  # type: ignore
     scoped_session, sessionmaker, relationship,
     backref)
+from sqlalchemy.dialects.postgresql import UUID, JSONB
 from sqlalchemy.ext.declarative import declarative_base  # type: ignore
 from sqlalchemy_utils import ChoiceType, Timestamp  # type: ignore
 
+import utils
 
-engine = create_engine('sqlite:///scrape.db')  # type: ignore
+
+engine = create_engine(utils.SCRAPEDB_URL)  # type: ignore
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
@@ -29,6 +32,6 @@ class ScrapedUrl(Base, Timestamp):  # type: ignore
     __tablename__ = 'scraped_url'
     id = Column(Integer, primary_key=True)
     url = Column(String, nullable=False)
-    data = Column(JSON, nullable=False)
+    data = Column(JSONB, nullable=False)
 
     Index('scraped_url_lookup', url, unique=True)
