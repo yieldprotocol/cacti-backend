dd1ce0d05c75988b63e1ccb5099949242813bafd
Kah Keng Tay <kahkeng@gmail.com>
2023-03-01 21:38:48 -0800

    tools: try to minimize conversing with widget tool by removing user mentions

--- chat/basic_agent.py
+++ chat/basic_agent.py
@@ -25,7 +25,7 @@ Action: the action/tool to use, should be one of [{tool_names}]
 Action Input: the input to the action/tool with the thought/intent and all details of relevant facts, matching the tool input format.
 Observation: the response to the {human_prefix} from the action.
 ... (this Thought/Action/Action Input/Observation can repeat N times)
-Thought: I have nothing more to say to {human_prefix}, or I need more input from the {human_prefix}.
+Thought: I have nothing more to say to {human_prefix}, or I need more input from the {human_prefix}, or the {human_prefix} has been shown a widget magic command.
 {ai_prefix}: DONE
 ```
 """
