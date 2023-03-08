55429ee8c69d3388dfea6e4dd803a10ba1e59628
Sagar Shah <shah.sagar.nilesh@gmail.com>
2023-03-08 11:12:13 -0600

    wip

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import registry
 
 widget_index = dict(
     type="index.weaviate.WeaviateIndex",
-    index_name="WidgetV3",
+    index_name="WidgetV4",
     text_key="content",
 )
 app_info_index = dict(
