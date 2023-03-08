078c4ebc82fae9be60492d27147a86ba31b4b157
Sagar Shah <shah.sagar.nilesh@gmail.com>
2023-03-08 11:47:47 -0600

    wip

--- tools/index_widget.py
+++ tools/index_widget.py
@@ -291,4 +291,42 @@ def nftasset(network: str, address: str, token_id: str) -> NFTAsset:
 
 @error_wrap
 def fetch_yields(token, chain, count) -> str:
-    return defillama.fetch_yields(token, chain, count)
+    # Convert the inferred canonical chain name to what DefiLlama uses for result filtering
+    if chain.lower() == "binance":
+        chain = "BSC"
+    elif chain.lower() == "mainnet":
+        chain = "Ethereum"
+
+    # If no inferred count, default to 5
+    if count == "*":
+        count = 5
+
+    yields = defillama.fetch_yields()
+    filtered_yields = list(filter(lambda yield_obj: _filter_yield_list(token, chain, yield_obj), yields))
+    filtered_yields.sort(key=lambda yield_obj: yield_obj["apy"], reverse=True)
+    selected_yields = filtered_yields[:int(count)]
+
+    answer = "Here are the Yields:"
+    for y in selected_yields:
+        data_obj = Yield(y["symbol"], y["chain"], y["project"], y["apy"])
+        answer += "\n{}".format(data_obj)
+
+    return answer
+
+
+def _filter_yield_list(input_token, input_chain, yield_obj) -> bool:
+    normalized_symbol = yield_obj["symbol"].lower()
+    normalized_chain = yield_obj["chain"].lower()
+    input_token = input_token.lower()
+    input_chain = input_chain.lower()
+
+    if input_token == "*" and input_chain == "*":
+        return True
+
+    if input_chain == "*":
+        return normalized_symbol == input_token
+
+    if input_token == "*":
+        return normalized_chain == input_chain
+
+    return normalized_symbol == input_token and normalized_chain == input_chain
