4685bbda7be0eb3def31e121a5461bafbe18a761
Scott Liao <dogboxx@gmail.com>
2023-03-27 12:39:33 -0700

    env: separate scrapedb into its own db instance

--- database/dbinit.sh
+++ database/dbinit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Run this via ./database/dbinit.sh
+# Run this via ENV_TAG=<env> ./database/dbinit.sh
+# where <env> is one of the environments defined within env/
+# (e.g. dev, prod)
 
 source ../venv/bin/activate
 python3 -c "from database.models import engine, Base; Base.metadata.create_all(bind=engine)"
