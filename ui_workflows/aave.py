e686be977ba82015b3147864550fad9d3afffb5b
Kah Keng Tay <kahkeng@gmail.com>
2023-04-04 15:52:55 -0700

    ui workflows: generalize aave to supply/borrow operations, add to widget index

--- ui_workflows/aave.py
+++ ui_workflows/aave.py
@@ -1,15 +1,20 @@
 from logging import basicConfig, INFO
 
-import registry
 from .base import BaseUIWorkflow
 
 basicConfig(level=INFO)
 
 
-@registry.register_class
 class AaveUIWorkflow(BaseUIWorkflow):
 
-    def run_page(self, page):
+    def __init__(self, wallet_chain_id: int, wallet_address: str, token: str, operation: str, amount: float) -> None:
+        super().__init__(wallet_chain_id, wallet_address)
+        assert operation in ("Supply", "Borrow"), operation
+        self.token = token
+        self.operation = operation
+        self.amount = amount
+
+    def _run_page(self, page):
         page.goto("https://app.aave.com/")
 
         # Intercept protocol's requests to its own RPC node
@@ -24,12 +29,17 @@ class AaveUIWorkflow(BaseUIWorkflow):
 
         self.start_listener(wc_uri)
 
-        # Supply/Lend 0.1 ETH
-        page.get_by_text("ETH", exact=True).first.click()
-        page.get_by_role("button", name="ETH", exact=True).click()
-        page.get_by_role("button", name="Supply").click()
-        page.get_by_placeholder("0.00").fill("0.1")
-        page.get_by_role("button", name="Supply ETH").click()
+        # Trigger operation
+        if self.operation == "Supply":
+            page.get_by_text(self.token, exact=True).first.click()
+            page.get_by_role("button", name=self.token, exact=True).click()
+        elif self.operation == "Borrow":
+            page.get_by_role("link", name=self.token).click()
+        else:
+            assert 0, f"unrecognized operation: {self.operation}"
+        page.get_by_role("button", name=self.operation).click()
+        page.get_by_placeholder("0.00").fill(str(self.amount))
+        page.get_by_role("button", name=f"{self.operation} {self.token}").click()
 
         # Arbitrary wait to allow WC to relay info to our client
         page.wait_for_timeout(5000)
@@ -39,8 +49,19 @@ class AaveUIWorkflow(BaseUIWorkflow):
         return result
 
 
+# Invoke this with python3 -m ui_workflows.aave
+
 if __name__ == "__main__":
     wallet_chain_id = 1  # Tenderly Mainnet Fork
     wallet_address = "0x5f5326CF5304fDD5c0B1308911D183aEc274536A"
-    wf = AaveUIWorkflow(wallet_chain_id, wallet_address)
+    token = "ETH"
+    operation = "Supply"
+    amount = 0.1
+    #wf = AaveUIWorkflow(wallet_chain_id, wallet_address, token, operation, amount)
+    #print(wf.run())
+
+    token = "USDC"
+    operation = "Borrow"
+    amount = 0.1
+    wf = AaveUIWorkflow(wallet_chain_id, wallet_address, token, operation, amount)
     print(wf.run())
