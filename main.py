f2d206aaec215e987d6409e105e788219bf19548
Kah Keng Tay <kahkeng@gmail.com>
2023-05-15 22:29:42 -0700

    backend: move all auth stuff to its own module

--- main.py
+++ main.py
@@ -1,28 +1,20 @@
 import asyncio
 from dataclasses import dataclass
-import json
-import time
-import traceback
-from typing import Any, Dict, List, Optional, Set, Union
+from typing import Optional, Set
 
 from fastapi import FastAPI, Request, Response, Body, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.middleware.sessions import SessionMiddleware
-import siwe
 
 import server
 import chat
 import env
-
-
-NONCE_EXPIRY_SECS = 60 * 60  # one hour
-AcceptJSON = Union[List, Dict, Any]  # a type that allows FastAPI to accept JSON objects
+import auth
 
 
 app = FastAPI()
 
 origins = env.env_config['server']['origins']
-host = env.env_config['server']['host']
 
 app.add_middleware(
     CORSMiddleware,
@@ -52,91 +44,17 @@ class ClientState:
 
 @app.get("/nonce")
 async def api_nonce(request: Request):
-    nonce = siwe.generate_nonce()
-    request.session["nonce"] = nonce
-    request.session["nonce_timestamp"] = time.time()
-    nonce = request.session.get("nonce")
-    nonce_timestamp = request.session.get("nonce_timestamp")
-    return nonce
-
-
-def _clear_session(request: Request):
-    request.session.pop("nonce", None)
-    request.session.pop("nonce_timestamp", None)
-    request.session.pop("wallet_address", None)
+    return auth.api_nonce(request)
 
 
 @app.post("/login")
-async def api_login(request: Request, data: AcceptJSON):
-    if not data:
-        _clear_session(request)
-        return
-    eip4361 = data.get("eip4361")
-    signature = data.get("signature")
-    if not eip4361 or not signature:
-        print('missing eip message or signature')
-        _clear_session(request)
-        return
-
-    eip4361 = json.loads(eip4361)
-    for python_key, javascript_key in [
-            ('chain_id', 'chainId'),
-            ('issued_at', 'issuedAt'),
-            ('expiration_time', 'expirationTime'),
-            ('not_before', 'notBefore'),
-            ('request_id', 'requestId'),
-    ]:
-        if javascript_key in eip4361:
-            eip4361[python_key] = eip4361.pop(javascript_key)
-
-    # check if we are already logged in, and it matches the address in the message
-    wallet_address = request.session.get("wallet_address")
-    if wallet_address and eip4361.get('address') == wallet_address:
-        print('wallet already authenticated', wallet_address)
-        # don't need to do anything, we only clear cookies if we explicitly log out
-        return True
-
-    nonce = request.session.get("nonce")
-    nonce_timestamp = request.session.get("nonce_timestamp")
-    if not nonce or not nonce_timestamp:
-        print('missing nonce or timestamp')
-        _clear_session(request)
-        return
-    if nonce_timestamp + NONCE_EXPIRY_SECS < time.time():
-        print('expired nonce')
-        _clear_session(request)
-        return
-
-    try:
-        # verify eip4361 and signature, get wallet address
-        message = siwe.SiweMessage(message=eip4361)
-        message.verify(signature, nonce=nonce, domain=host)
-        assert message.statement == 'Sign me in to wc3 app', message.statement
-        wallet_address = message.address
-        print('authenticated wallet', wallet_address)
-    except (
-            siwe.VerificationError,
-            siwe.InvalidSignature,
-            siwe.ExpiredMessage,
-            siwe.NotYetValidMessage,
-            siwe.DomainMismatch,
-            siwe.NonceMismatch,
-            siwe.MalformedSession,
-            AssertionError,
-    ):
-        traceback.print_exc()
-        _clear_session(request)
-        return
-
-    # set authenticated wallet address in session
-    request.session["wallet_address"] = wallet_address
-    return True
+async def api_login(request: Request, data: auth.AcceptJSON):
+    return auth.api_login(request, data)
 
 
 @app.post("/logout")
 async def api_logout(request: Request):
-    _clear_session(request)
-    return True
+    return auth.api_logout(request)
 
 
 @app.websocket("/chat")
@@ -157,8 +75,9 @@ async def _handle_websocket(websocket: WebSocket):
         try:
             message = await websocket.receive_text()
 
-            # Handle authenticated wallet address, if any (including changes)
-            client_state.wallet_address = websocket.session.get("wallet_address")
+            # Fetch authenticated wallet address from the session cookies. If
+            # not authenticated, this is None, and we handle this inside
+            client_state.wallet_address = auth.fetch_authenticated_wallet_address(websocket)
 
             queue = asyncio.queues.Queue()
 
