7eb80538f1ac63740b60f031552a615994902d57
Sagar Shah <shah.sagar.nilesh@gmail.com>
2023-06-08 13:50:44 -0500

    wip

--- ui_workflows/conftest.py
+++ ui_workflows/conftest.py
@@ -5,17 +5,17 @@ import requests
 import pytest
 
 import context
-from utils import create_fork, remove_fork
+from utils import create_fork, remove_fork, TEST_TENDERLY_FORK_ID
 
 @pytest.fixture(scope="module")
 def setup_fork():
     # Before test
-    #fork_id = create_fork()
-    fork_id = "da6416f8-c838-4c8c-8215-47d2710df1ee"
+    fork_id = TEST_TENDERLY_FORK_ID or create_fork()
 
     with context.with_request_context(None, None, wallet_chain_id=None, fork_id=fork_id):
         # Return to test function
         yield {"fork_id": fork_id}
     
     # After test
-    #remove_fork(fork_id)
+    if not TEST_TENDERLY_FORK_ID:
+        remove_fork(fork_id)
