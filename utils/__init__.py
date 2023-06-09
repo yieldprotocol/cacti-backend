4f8b2ddfb43e992b884fd2659edea1857408bc45
Scott Liao <dogboxx@gmail.com>
2023-06-09 16:25:09 -0700

    utils: unentangle storage address constants

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,6 +2,7 @@ from .constants import *
 from .common import *
 from .abi import *
 from .crypto_token import *
+from .storage import *
 from .tenderly import *
 
 from . import web3_provider
--- utils/common.py
+++ utils/common.py
@@ -8,21 +8,8 @@ from ens import ENS
 import functools
 import traceback
 import context
-import env
 
 from .constants import OPENAI_API_KEY, TENDERLY_FORK_URL
-
-def _get_weaviate_url(config):
-    return f"{config.get('protocol', 'https')}://{config['user']}:{config['password']}@{config['host']}:{config['port']}"
-
-
-def _get_postgres_url(config, database_name):
-    return f"postgresql://{config['user']}:{config['password']}@{config['host']}:{config['port']}/{database_name}"
-
-
-WEAVIATE_URL = _get_weaviate_url(env.env_config['weaviate'])
-CHATDB_URL = _get_postgres_url(env.env_config['chatdb'], 'chatdb')
-SCRAPEDB_URL = _get_postgres_url(env.env_config['scrapedb'], 'scrapedb')
 
 
 def set_api_key():
