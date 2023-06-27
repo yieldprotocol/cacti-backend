1e3080d687eca7ef781c9515bf56fa847c6440b5
Kah Keng Tay <kahkeng@gmail.com>
2023-06-27 08:51:02 -0700

    readme: clarify how to bring up local db schema with alembic

--- database/dbinit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run this via ENV_TAG=<env> ./database/dbinit.sh
-# where <env> is one of the environments defined within env/
-# (e.g. dev, prod)
-
-source ../venv/bin/activate
-python3 -c "from database.models import engine, Base; Base.metadata.create_all(bind=engine)"
