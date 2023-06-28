d8a112ed1c3bfda0beb2ffb0a23d1fa680f72b93
Kah Keng Tay <kahkeng@gmail.com>
2023-06-27 22:16:42 -0700

    alembic: checks to ensure we are on the right schema
    If we have a migration that isn't applied yet, we won't be able to start the backend.
    If we have changes to models without associated alembic migration, unit tests will fail.

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check the current alembic revision, and see if it matches the head revision.
+if [[ -z "${SKIP_DB_CHECK}" ]]
+then
+    echo "Checking current database revision..."
+    alembic current | grep head || { echo "WARNING: Database schema not in sync. Run \"ENV_TAG=$ENV_TAG alembic upgrade head\" to update the $ENV_TAG database, or prepend SKIP_DB_CHECK=1 to your command to ignore this warning."; exit 1; }
+fi
+
 xvfb_cmd=xvfb-run
 start_cmd="uvicorn main:app --host 0.0.0.0 --port 9999"
 
