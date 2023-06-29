bca8d6a6ee2b2db96804ee9a717cc10b4fbf63b2
Sagar Shah <shah.sagar.nilesh@gmail.com>
2023-06-29 14:56:16 -0500

    wip

--- README.md
+++ README.md
@@ -31,3 +31,4 @@ ENV_TAG=local ./db_schema_sync.sh
 - Increment the numeric version in `WIDGET_INDEX_NAME` constant in `utils/constants.py`
 - For local env, the widget index name would use your OS login name to create an isolated index. For dev/prod, the widget index would be the numeric version mentioned above. (more info in `scripts/update_widget_index.py`)
 - Run this Python command to update our Weaviate Vector DB with the new widget `python -m scripts.update_widget_index`
+- Ensure textual translation for the widget command is added to the `_widgetize_inner` function in `display_widget.py` file
--- scripts/check_widget_translation.py
+++ scripts/check_widget_translation.py
@@ -3,16 +3,20 @@ from chat.display_widgets import parse_widgets_into_text
 from index.widgets import backfill, get_client as weviate_get_client, TEXT_KEY
 from utils import get_widget_index_name
 
-RE_WIDGET_COMMAND = re.compile(r"<\|.*?\|>")
-RE_PARAMS = re.compile(r"\{.*?\}")
+# Widgets file is the canonical source
+WIDGETS_FILE = 'knowledge_base/widgets.txt'
+
 BATCH_SIZE = 20
 CLASS_PROPERTIES = [TEXT_KEY]
+
+RE_WIDGET_COMMAND = re.compile(r"<\|.*?\|>")
+RE_PARAMS = re.compile(r"\{.*?\}")
 
 def get_batch_with_offset(client, class_name, class_properties, batch_size, offset):
     query = client.query.get(class_name, class_properties).with_offset(offset).with_limit(batch_size)
     return query.do()
 
-def main():
+def read_widgets_from_index():
     index_name = get_widget_index_name()
     all_content = []
     offset = 0
@@ -27,15 +31,32 @@ def main():
             all_content.append(r[TEXT_KEY])
 
         offset += BATCH_SIZE
+    return all_content
 
-    for c in all_content:
-        matches = RE_WIDGET_COMMAND.findall(c)
+def read_widgets_from_file():
+    with open(WIDGETS_FILE, 'r') as f:
+        all_content = f.read()
+    
+    return all_content.split('---')
+
+def check_widgets_for_textual_translation(widgets, is_from_index):
+    for w in widgets:
+        matches = RE_WIDGET_COMMAND.findall(w)
         widget_command = matches[0]
         if 'display-' in widget_command:
             filled_widget_command = RE_PARAMS.sub('NA', widget_command)
             text = parse_widgets_into_text(filled_widget_command)
+            error_type = 'Widget Index Error' if is_from_index else 'Widget File Error'
             if 'An unrecognized command' in text:
-                raise Exception(f'Textual translation missing in _widgetize_inner function in display_widget.py file for command: {widget_command}')
+                raise Exception(f"{error_type}: textual translation missing in _widgetize_inner function in display_widget.py file for command: {widget_command}")  
+
+def main():
+    widgets_from_index = read_widgets_from_index()
+    widgets_from_file = read_widgets_from_file()
+
+    check_widgets_for_textual_translation(widgets_from_index, True)
+    check_widgets_for_textual_translation(widgets_from_file, False)
+
 
 if __name__ == '__main__':
     main()
