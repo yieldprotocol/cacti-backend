f1b8bd153406e2cfd7f42a0df8d2d1c01da0a297
marcomariscal <marco.a.mariscal@gmail.com>
2023-07-11 10:13:46 -0700

    feat: yp borrow

--- chat/display_widgets.py
+++ chat/display_widgets.py
@@ -144,6 +144,9 @@ def _widgetize_inner(command: str, params: str, depth: int = 0) -> str:
     elif command == 'display-yield-protocol-lend-close':
         params = json.loads(params)
         lines.append(f"yield protocol lend close action for token: {params['token']}, amount: {params['amount']}.")
+    elif command == 'display-yield-protocol-borrow':
+        params = json.loads(params)
+        lines.append(f"yield protocol borrow action for borrow token: {params['borrowToken']}, borrow amount: {params['borrowAmount']}, collateral token: {params['collateralToken']}, collateral amount: {params['collateralAmount']}.")
     else:
         # assert 0, f'unrecognized command: {command}({params})'
         lines.append(f"An unrecognized command: {command}({params})")
