99c240c27fad88e59f1aafce8612a4b01be3dcd9
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2023-07-19 19:03:49 -0500

    fix: make text uniform

--- chat/display_widgets.py
+++ chat/display_widgets.py
@@ -140,7 +140,7 @@ def _widgetize_inner(command: str, params: str, depth: int = 0) -> str:
         lines.append(f"ZkSync bridge withdraw action for token: {params['token']}, amount: {params['amount']}.")
     elif command == 'stake-sfrxeth':
         params = json.load(params)
-        lines.append(f"sfrxETH mint action for address: {params['receiver']}, amount: {params['value']}.")
+        lines.append(f"sfrxETH deposit action for address: {params['receiver']}, amount: {params['value']}.")
     else:
         # assert 0, f'unrecognized command: {command}({params})'
         lines.append(f"An unrecognized command: {command}({params})")
