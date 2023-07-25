001c2d431c8553298f6ba8dd1f7989b7e84d4c89
Sabnock01 <24715302+Sabnock01@users.noreply.github.com>
2023-07-25 10:07:48 -0500

    feat: add arbitrum eth transfer integration

--- chat/display_widgets.py
+++ chat/display_widgets.py
@@ -138,6 +138,12 @@ def _widgetize_inner(command: str, params: str, depth: int = 0) -> str:
     elif command == 'zksync-withdraw':
         params = json.loads(params)
         lines.append(f"ZkSync bridge withdraw action for token: {params['token']}, amount: {params['amount']}.")
+    elif command == 'arbitrum-deposit-eth':
+        params = json.load(params)
+        lines.append(f"Arbitrum bridge deposit action for ETH amount: {params['amount']}.")
+    elif command == 'arbitrum-withdraw-eth':
+        params = json.load(params)
+        lines.append(f"Arbitrum bridge withdraw action for ETH amount: {params['amount']}.")
     else:
         # assert 0, f'unrecognized command: {command}({params})'
         lines.append(f"An unrecognized command: {command}({params})")
