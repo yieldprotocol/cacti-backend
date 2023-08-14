8ad1ceac015ff9c9867cc7df0a9f7e830b1e8e36
Sagar Shah <shah.sagar.nilesh@gmail.com>
2023-08-14 16:13:07 -0500

    Changed casing for current eth to ETH

--- integrations/opensea.py
+++ integrations/opensea.py
@@ -100,7 +100,7 @@ def fetch_listings(address: str, token_id: str) -> List[NFTListing]:
     )
     for item in obj['orders']:
         price_value = int(item["current_price"])
-        currency = "eth"
+        currency = "ETH"
         price_str = f"{utils.w3.from_wei(price_value, 'ether')} {currency}"
         listing = NFTListing(
             chain=chain,
