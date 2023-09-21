af0bc08f3dd7cd43d036b05921ec42ec79bf672d
Prafful <46891804+iamsahu@users.noreply.github.com>
2023-09-21 21:06:21 +0200

    Generalized 4626 vault integration (#161)
    * Update widgets.yaml
    
    * feat: deposit vault widget
    
    * Update widgets.yaml
    
    * Update widgets.yaml
    
    * Update display_widgets.py
    
    * Update display_widgets.py
    
    * Update display_widgets.py
    
    * fix: move to end
    
    * fix: use items
    
    * chore: widget index bump
    
    ---------
    
    Co-authored-by: marcomariscal <marco.a.mariscal@gmail.com>

--- chat/display_widgets.py
+++ chat/display_widgets.py
@@ -189,6 +189,18 @@ def _widgetize_inner(command: str, params: str, depth: int = 0) -> str:
     elif command == "withdraw-eth-reth":
         items = params.split(",")
         lines.append(f"withdraw eth from reth action for amount: {items[0]}.")
+    elif command == 'savings-dai-deposit':
+        items = params.split(",")
+        lines.append(f"DAI deposit action for amount: {items[0]}.")
+    elif command == 'savings-dai-withdraw':
+        items = params.split(",")
+        lines.append(f"DAI withdraw action for amount: {items[0]}.")
+    elif command == 'deposit-vault':
+        items = params.split(",")
+        lines.append(f"Deposit vault action for token: {items[0]}, amount: {items[1]}.")
+    elif command == 'withdraw-vault':
+        items = params.split(",")
+        lines.append(f"Withdraw vault action for token: {items[0]}, amount: {items[1]}.")
     else:
         # assert 0, f'unrecognized command: {command}({params})'
         lines.append(f"An unrecognized command: {command}({params})")
