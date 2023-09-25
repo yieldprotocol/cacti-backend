c49a1e6328b2a0006a7b51cba78b15f94941e607
marcomariscal <marco.a.mariscal@gmail.com>
2023-09-25 11:00:57 -0700

    fix: spelling

--- chat/display_widgets.py
+++ chat/display_widgets.py
@@ -203,7 +203,7 @@ def _widgetize_inner(command: str, params: str, depth: int = 0) -> str:
         lines.append(f"Withdraw vault action for token: {items[0]}, amount: {items[1]}.")
     elif command == 'code-runner':
         items = params.split(",")
-        lines.append(f"supply the user with valid javascript code to run and perform their desicred action.")
+        lines.append(f"supply the user with valid javascript code to run and perform their desired action.")
     else:
         # assert 0, f'unrecognized command: {command}({params})'
         lines.append(f"An unrecognized command: {command}({params})")
