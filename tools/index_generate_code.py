c995addf907adaac8c2e237cf817cfd8be0c6658
marcomariscal <marco.a.mariscal@gmail.com>
2023-09-26 09:48:53 -0700

    fix: inherit from base tool

--- tools/index_generate_code.py
+++ tools/index_generate_code.py
@@ -1,10 +1,10 @@
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
+from pydantic import Extra
 
 import registry
 import streaming
-from gpt_index.utils import ErrorToRetry, retry_on_exceptions_with_backoff
-import utils.timing as timing
+from .base import BaseTool, BASE_TOOL_DESCRIPTION_TEMPLATE
 
 
 TEMPLATE = '''You are an expert Web3 developer well versed in using JS to interact with the ecosystem, you will help the user perform actions based on their request by generating functional JS code
@@ -19,19 +19,19 @@ TEMPLATE = '''You are an expert Web3 developer well versed in using JS to intera
 # USER REQUEST EXAMPLE
 write code to send 2 eth to 0x123
 
----
-{task_info}
----
-
 User: {question}
 Assistant:'''
 
 
 @registry.register_class
-class IndexGenerateCodeTool():
+class IndexGenerateCodeTool(BaseTool):
     """Tool for generating code to perform a user request."""
 
     _chain: LLMChain
+
+    class Config:
+        """Configuration for this pydantic object."""
+        extra = Extra.allow
 
     def __init__(
             self,
@@ -39,29 +39,32 @@ class IndexGenerateCodeTool():
             **kwargs
     ) -> None:
         prompt = PromptTemplate(
-            input_variables=["task_info", "question"],
+            input_variables=["question"],
             template=TEMPLATE,
         )
         new_token_handler = kwargs.get('new_token_handler')
         chain = streaming.get_streaming_chain(prompt, new_token_handler)
+        description=BASE_TOOL_DESCRIPTION_TEMPLATE.format(
+                tool_description="generate code based on the user query",
+                input_description="a standalone query where user wants to generate code to perform an action",
+                output_description="a message answer, along with the generated code with helpful comments",
+            )
+
         super().__init__(
             *args,
             _chain=chain,
-            input_description="a request that the user wants to perform using code",
-            output_description="generated javascript code",
+            description=description,
             **kwargs
         )
 
     def _run(self, query: str) -> str:
-
-        retry_on_exceptions_with_backoff(
-            lambda: self._index.similarity_search(query, k=self._top_k),
-            [ErrorToRetry(TypeError)],
-        )
-
-        timing.log('widget_index_lookup_done')
-
-        self._chain.verbose = True
-        result = self._chain.run()
+        example = {
+            "question": query,
+            "stop": "User",
+        }
+        result = self._chain.run(example)
 
         return result
+
+    async def _arun(self, query: str) -> str:
+        raise NotImplementedError(f"{self.__class__.__name__} does not support async")
